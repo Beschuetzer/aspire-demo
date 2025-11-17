5a12e9d6f282bb6d35feeba599f1484d18621107
Adam <62818816+Beschuetzer@users.noreply.github.com>
2025-11-17 12:14:34 -0600

    chore: run azd init

--- AspireDemo.AppHost/AppHost.cs
+++ AspireDemo.AppHost/AppHost.cs
@@ -1,5 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add the following line to configure the Azure App Container environment
+builder.AddAzureContainerAppEnvironment("env");
+
 var apiService = builder.AddProject<Projects.AspireDemo_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
